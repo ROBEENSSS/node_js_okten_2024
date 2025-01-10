e5cff7de6a41cff836667726902710556e701e4b
ROBENSS <rmalhasan6@gmail.com>
2025-01-10 21:45:36 +0200

    start HW3

---
+++ fs.service.js
@@ -0,0 +1,16 @@
+module.exports = {
+    read: async () => {
+        try {
+
+        } catch (error) {
+            console.error('Error:',error.message);
+        }
+    },
+    write: async () => {
+        try {
+
+        } catch (error) {
+            console.error('Error:',error.message);
+        }
+    }
+}
